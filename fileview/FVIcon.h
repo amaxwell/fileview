e841a81763298165e9f980006d7b52347ce1a2d7
adam.r.maxwell <amaxwell@mac.com>
2008-07-06 02:03:49 +0000

    Modify comments for compatibility with doxygen.

--- fileview/FVIcon.h
+++ fileview/FVIcon.h
@@ -38,56 +38,100 @@
 
 #import <Cocoa/Cocoa.h>
 
+/** Abstract class used for drawing in FileView
+ 
+ FVIcon is a class cluster.  You should typically never receive an instance of FVIcon from its initializer, but will instead get an instance of a concrete subclass that correctly handles a given URL scheme or file type.
+
+ The iconWithURL: factory method is designed to be cheap, in that it does no rendering, should will require very little memory or disk access just for initialization.  Only after calling renderOffscreen will memory usage increase substantially, as data is cached and bitmaps created.  Icons that won't be displayed for some time (scrolled out of sight) should be sent a releaseResources message by the view in order to free up (some) of the cached data.  Subsequent calls to renderOffscreen should be substantially less expensive, since data will be read from the disk cache.
+ 
+ This class is thread safe, but it is not reentrant.  You can abuse it to create deadlocks.  Don't do that. */
 @interface FVIcon : NSObject
 
-/*
- Note: the +iconWith... class methods are designed to be cheap, in that they do no rendering, and will require very little memory or disk access just for initialization.  Only after calling -renderOffscreen will memory usage increase substantially, as data is cached and bitmaps created.  Icons that won't be displayed for some time (scrolled out of sight) should be sent a -releaseResources message by the view in order to free up (some) of the cached data.
+/** Shared instance representing a missing file.
  
- This class is thread safe, at least within reason.
- */
-
-// Returns a URL that is appropriate for a missing file.  Don't rely on the scheme or path for anything.
+ @return A URL that is appropriate for a missing file to be passed to iconWithURL:.  Don't rely on the scheme or path for anything. */
 + (NSURL *)missingFileURL;
 
-// Will accept any URL type and return a file thumbnail, file icon, or appropriate icon for the given URL scheme.
+/** Only public factory method.
+ 
+ Decides which concrete subclass to return based on the scheme and/or UTI of the URL or its target.
+ @param representedURL Any URL type
+ @return A file thumbnail, file icon, or appropriate icon for the given URL scheme. */
 + (id)iconWithURL:(NSURL *)representedURL;
 
-// Possibly releases cached resources for icons that won't be displayed.  The only way to guarantee a decrease in memory usage is to release all references to the object, though, as this call may be a noop for some subclasses.
+/** Releases cached resources.
+ 
+ Send this to icons that won't be displayed "soon."  The only way to guarantee a decrease in memory usage is to release all references to the object, though, as this call may be a noop for some subclasses. */
 - (void)releaseResources;
+
+/** Determine if releaseResources is possible.
+ 
+ @return NO if releaseResources will be a no-op or otherwise is not possible. */
 - (BOOL)canReleaseResources;
 
-// Returns NO if the icon already has a cached version for this size; if it returns YES, this method sets the desired size in the case of Finder icons, and the caller should then send -renderOffscreen from the render thread.
+/** Determine if renderOffscreen is required.
+ 
+ Clients (i.e. FileView) calls this in order to see if renderOffscreen should be called.  If it returns YES, this method sets the desired size in the case of Finder icons, and the caller should then send renderOffscreen.  By the same token, if this returns NO, don't waste time on renderOffscreen.
+ 
+ @param size The desired icon size in points.  Subclasses are free to ignore this.
+ @return NO if the icon already has a cached version for this size. */
 - (BOOL)needsRenderForSize:(NSSize)size;
 
-// Renders the icon into an offscreen bitmap context; should be called from a dedicated rendering thread after needsRenderForSize: has been called.  This call may be expensive, but it's required for correct drawing.
+/** Draws the icon into an offscreen bitmap context.
+ 
+ This is typically the most expensive call for an FVIcon subclass.  In general it should be called from a dedicated thread after needsRenderForSize: has been called, unless you're planning to draw synchronously.  This is required for correct drawing, since a placeholder will typically be drawn if the bitmap is not available. */
 - (void)renderOffscreen;
 
-/*
- - the image will be scaled proportionally and centered in the destination rect
- - the view is responsible for using -centerScanRect: as appropriate, but images may not end up on pixel boundaries
- - any changes to the CGContextRef are wrapped by CGContextSaveGState/CGContextRestoreGState
- - specific compositing operations should be set in the context before calling this method
- - shadow will be respected (the clip path is only changed when rendering text)
- - needsRenderForSize: and renderOffscreen must be called first, to check/set size
- - don't bother calling -renderOffscreen if -needsRenderForSize: returns NO
- - a placeholder icon will be drawn if -renderOffscreen has not been called or finished working
- */
+/** Primitive drawing method.
+ 
+ Subclasses must override this.  The drawing has the following semantic requirements and guarantees at present:
+ 
+ \li \c the image will be scaled proportionally and centered in the destination rect
+ \li \c the view is responsible for using -[NSView centerScanRect:] as appropriate, but images may not end up on pixel boundaries
+ \li \c any changes to the CGContextRef are wrapped by CGContextSaveGState/CGContextRestoreGState
+ \li \c specific compositing operations should be set in the context before calling this method
+ \li \c shadow will be respected (the clip path is only changed when rendering text)
+ \li \c needsRenderForSize: and renderOffscreen must be called first, to check/set size
+ \li \c a placeholder icon will be drawn if renderOffscreen has not been called or finished working
+ 
+ @param dstRect Destination rect for drawing in the passed-in context's coordinate space.
+ @param context CGContext for drawing content. */
 - (void)drawInRect:(NSRect)dstRect ofContext:(CGContextRef)context;
 
-// fastDrawInRect: draws a lower quality version if available, using the same semantics as drawInRect:ofContext:
+/** Optional drawing method.
+ 
+ Draws a lower quality version if available, using the same semantics as FVIcon::drawInRect:ofContext:.
+ @param dstRect Destination rect for drawing in the passed-in context's coordinate space.
+ @param context CGContext for drawing content. */ 
 - (void)fastDrawInRect:(NSRect)dstRect ofContext:(CGContextRef)context;
 
-// get rid of any cached representations; next time the icon is redrawn, its data will be reloaded
+/** Purge bitmap caches.
+ 
+ Get rid of any cached representations; next time the icon is redrawn, its data will be recreated in renderOffscreen. */
 - (void)recache;
 
 @end
 
 @interface FVIcon (Pages)
 
-// The -currentPageIndex return value is 1-based, as in CGPDFDocumentGetPageCount; only useful for multi-page formats such as PDF and PS.  Multi-page TIFFs and text documents are not supported, and calling the showNextPage/showPreviousPage methods will have no effect.
+/** Number of pages if multipage drawing is supported.
+ 
+ Only useful for multi-page formats such as PDF and PS.  Multi-page TIFFs and text documents are not supported, and calling the showNextPage/showPreviousPage methods will have no effect. 
+ @return Number of pages available for drawing. */
 - (NSUInteger)pageCount;
+/** The index of the current page.
+ 
+ @return The return value is 1-based, as in CGPDFDocumentGetPageCount. */
 - (NSUInteger)currentPageIndex;
+
+/** Increments the internal page index.
+ 
+ This does not redisplay the icon; needsRenderForSize: and renderOffscreen must be called to redraw. */
 - (void)showNextPage;
+
+/** Decrements the internal page index.
+ 
+ This does not redisplay the icon; needsRenderForSize: and renderOffscreen must be called to redraw. */
 - (void)showPreviousPage;
 
 @end
