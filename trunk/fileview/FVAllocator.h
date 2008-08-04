05290883bafdd97c6197f834ba65e04960723cc0
adam.r.maxwell <adam.r.maxwell@051f3cb3-f650-0410-be63-fd85f1a41f84>
2008-08-04 22:29:23 +0000

    Add documentation for FVAllocator

--- trunk/fileview/FVAllocator.h
+++ trunk/fileview/FVAllocator.h
@@ -38,5 +38,24 @@
 
 #import <Cocoa/Cocoa.h>
 
-FV_PRIVATE_EXTERN void FVAllocatorShowStats(void);
+/** @file FVAllocator.h @brief Allocator for moderate-to-large blocks.
+ 
+ This allocator is primarily intended for use when many blocks >16K are needed repeatedly.  Each block is retained in a cache for some time after being freed, so reuse of similarly sized blocks should be fast.  Blocks of memory returned are not zeroed; the caller is responsible for this as needed.  In fact, a primary advantage of this allocator is that it doesn't waste time zeroing memory before returning it.  Typical usage is to provide a block of memory for a CGBitmapContext, vImage_Buffer, or backing for a CGDataProvider.  In the latter case, you can use CFDataCreateWithBytesNoCopy()/CGDataProviderCreateWithCFData() to good advantage, particularly for repeated creation/destruction of short-lived/same-sized images.
+ 
+ This is <b>not</b> a general-purpose replacement for the system allocator(s), and the code doesn't draw from tcmalloc or Apple's malloc implementation.  There's no guarantee of good scalability with this, but it's quite fast with a few hundred cached pointers, and doesn't spend time in spinlocks like Apple's malloc does.  If profiling turns up bottlenecks, there'll likely be some obvious wins; using a hash table instead of a binary search, for example.
+ 
+ For some background on the problem, see this thread:  http://lists.apple.com/archives/perfoptimization-dev/2008/Apr/msg00018.html which indicates that waiting for a solution from Apple is probably not going to be very gratifying. 
+ 
+ @warning If allocations are sized such that you can't reuse them, this allocator is not for you.
+ 
+ */
+
+/** @internal @brief Allocator for moderate-to-large blocks.
+ 
+ The allocator is thread-safe.
+ @return The shared allocator instance. */
 FV_PRIVATE_EXTERN CFAllocatorRef FVAllocatorGetDefault(void);
+
+/** @internal @brief Print allocator statistics.
+ Logs various information about allocator usage, particularly the number and size of the free blocks currently available.  This should be used for debugging only.  */
+FV_PRIVATE_EXTERN void FVAllocatorShowStats(void);
