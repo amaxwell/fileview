52f23db27224ca5777c95c0571a727b18c0f8997
adam.r.maxwell <amaxwell@mac.com>
2009-01-26 05:22:51 +0000

    Fix copyright notices that were wiped out by Xcode crashes.

--- fileview/fv_zone.cpp
+++ fileview/fv_zone.cpp
@@ -6,7 +6,7 @@
  *
  */
 /*
- This software is 
+ This software is Copyright (c) 2008-2009
  Adam Maxwell. All rights reserved.
  
  Redistribution and use in source and binary forms, with or without
