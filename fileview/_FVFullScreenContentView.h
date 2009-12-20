7c9af59ad4404c54cf111a8e5a16b466f571903b
adam.r.maxwell <amaxwell@mac.com>
2009-12-20 19:08:24 +0000

    add documentation for new classes

--- fileview/_FVFullScreenContentView.h
+++ fileview/_FVFullScreenContentView.h
@@ -40,6 +40,11 @@
 
 @class FVPreviewer;
 
+/** @internal @brief Full screen window content view.
+ 
+ Forwards an esc key event to the window's delegate by invoking -(void)cancel:(id)sender when in
+ fullscreen mode, since the normal responder chain won't work.  For consistency, also does this when
+ not in fullscreen mode. */
 @interface _FVFullScreenContentView : NSView
 {
     FVPreviewer *_windowDelegate;
