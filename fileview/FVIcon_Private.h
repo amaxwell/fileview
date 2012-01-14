87bc6629d46d70113c26a066198a5cd989294b86
adam.r.maxwell <amaxwell@mac.com>
2012-01-14 19:20:36 +0000

    update copyright

--- fileview/FVIcon_Private.h
+++ fileview/FVIcon_Private.h
@@ -6,7 +6,7 @@
  *
  */
 /*
- This software is Copyright (c) 2007-2011
+ This software is Copyright (c) 2007-2012
  Adam Maxwell. All rights reserved.
  
  Redistribution and use in source and binary forms, with or without
