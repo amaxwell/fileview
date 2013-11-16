628dc8af8033abdbfac0e280174d58eafb33bf2d
adam.r.maxwell <amaxwell@mac.com>
2013-11-16 05:45:41 +0000

    update copyrights

--- fileview/FVGCDOperationQueue.m
+++ fileview/FVGCDOperationQueue.m
@@ -4,7 +4,7 @@
 //
 //  Created by Adam Maxwell on 3/14/12.
 /*
- This software is Copyright (c) 2012
+ This software is Copyright (c) 2012-2013
  Adam Maxwell. All rights reserved.
  
  Redistribution and use in source and binary forms, with or without
